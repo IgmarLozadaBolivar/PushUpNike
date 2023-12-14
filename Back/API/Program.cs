efe2ee97e490ddefe5e5c3b8def850775c63aeeb
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-14 16:33:02 -0500

    feat: :sparkles: Changes in the program file

--- Back/API/Program.cs
+++ Back/API/Program.cs
@@ -1,19 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(builder.Configuration)
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+
+builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DbAppContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("ConexMysql");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<DbAppContext>();
+        await context.Database.MigrateAsync();
+        await DbAppContextSeed.SeedRolesAsync(context, loggerFactory);
+        await DbAppContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var _logger = loggerFactory.CreateLogger<Program>();
+        _logger.LogError(ex, "Ocurrio un error durante la migracion!");
+    }
 }
 
 app.UseHttpsRedirection();
