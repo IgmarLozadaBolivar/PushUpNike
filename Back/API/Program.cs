38c7e027373da1a703fcf0b6a3f6bda69f1abe1d
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-12-14 17:12:00 -0500

    feat: :sparkles: New add-ons for the correct functionality of the application

--- Back/API/Program.cs
+++ Back/API/Program.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+using API.Extensions;
+using API.Helpers;
+using AspNetCoreRateLimit;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Persistence;
 using Serilog;
 
@@ -12,8 +17,34 @@ var logger = new LoggerConfiguration()
 builder.Logging.AddSerilog(logger);
 
 builder.Services.AddControllers();
+builder.Services.AddAplicacionServices();
+builder.Services.ConfigureRatelimiting();
+builder.Services.ConfigureApiVersioning();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PushUp Nike", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.ConfigureCors();
 
 builder.Services.AddDbContext<DbAppContext>(options =>
 {
@@ -21,12 +52,19 @@ builder.Services.AddDbContext<DbAppContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+builder.Services.AddJwt(builder.Configuration);
+
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "PushUp Nike v1");
+        }
+    );
 }
 
 using (var scope = app.Services.CreateScope())
@@ -47,7 +85,13 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseHttpsRedirection();
+
+app.UseIpRateLimiting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
